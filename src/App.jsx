aefa89c306d2d946b02eda2538ac82edfeaf9ed8
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-09-29 15:20:30 +0100

    novo js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { useState } from 'react';
+import Prodouts from './pages/itnes'
+import { Inical_page } from './pages/inicial';
+import './css/menu.css'
+import './css/but.css';
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const menuItems = ['Página Inicial', 'Página 1', 'Página 2', 'Sobre nós'];
+
+  const handleMenuItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  let content = null;
+
+  switch (selectedItem) {
+    case 'Página Inicial':
+      content = <> 
+      
+      <Inical_page/>
+      
+      </>;
+      
+      break;
+    case 'Página 1':
+      content = <> <h2>Conteúdo da Página 1</h2>;
+       
+      </>
+      break;
+    case 'Página 2':
+      content = <h2>Conteúdo da Página 2</h2>;
+      break;
+    case 'Sobre nós':
+      content = <h2>sober nos</h2>;
+      break;
+    default:
+      content = <> <h2>Selecione uma Página</h2>  
+      
+         <Prodouts/>
+         </>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+
+ 
+        
+    
+      
+      <nav>
+      <ul className='Ule' >
+        {menuItems.map((item, index) => (
+          <li className='intem_menu'
+            type="button" class="btn btn-light"
+            key={index}
+            onClick={() => handleMenuItemClick(item)}
+            // className={selectedItem === item ? 'selected' : ''}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+      </nav>
+      
+      <div>{content}</div>
+      
+     
+    </div>
+  );
 }
 
-export default App
+export default App;
