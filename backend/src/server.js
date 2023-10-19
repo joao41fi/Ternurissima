5aae1b937c8f1de276773d699f68853b2872c5d0
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-10-19 16:12:55 +0100

    ja excutou

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const routers = require('./routes');
 
+const listAllFiles = require('./api_3s.cjs');
+
 app.use(routers);
 
 app.use(express.json());
@@ -12,5 +14,17 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('la mundo')
+    console.log('la mundo');
+    listAllFiles()
+  .then(files => {
+    // Faça algo com a lista de arquivos, por exemplo:
+    console.log('Total de arquivos encontrados:', files.length);
+  })
+  .catch(error => {
+    console.error('Erro ao listar arquivos:', error);
+  });
+
+
 })
+
+
